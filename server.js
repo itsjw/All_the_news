cc35d027648057e16a6880719bfef042c8d8b0a0
Dwayne Obenshain <bearmech1@gmail.com>
2017-12-09 11:07:15 -0600

    once again

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/scrape", function(req, res) {
       // If this found element had both a title and a link
       if (title && link) {
         // Insert the data in the scrapedData db
-        db.save.insert({
+        db.Article.insert({
           title: title,
           link: link
         },
