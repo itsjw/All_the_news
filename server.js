fb23cc3d47fbbed4c9bf70f96937e8dd918e986b
Dwayne Obenshain <bearmech1@gmail.com>
2017-12-28 15:19:43 -0600

    changed url

--- server.js
+++ server.js
@@ -67,11 +67,11 @@ app.get("/name", function(req, res) {
 app.get("/scrape", function(req, res) {
 
   // Make a request for the news section of ycombinator
-  request("http://www.thesmokinggun.com//", function(error, response, html) {
+  request("http://https://www.drudge.com//", function(error, response, html) {
     // Load the html body from request into cheerio
     var $ = cheerio.load(html);
     // For each element with a "title" class
-    $(".field-content").each(function(i, element) {
+    $(".frontheadline").each(function(i, element) {
       // Save the text and href of each link enclosed in the current element
       var title = $(element).children("a").text();
       var link = $(element).children("a").attr("href");
